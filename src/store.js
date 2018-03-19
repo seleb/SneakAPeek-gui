08eea3eb6a2df09339a580820b6bed46aa8536bf
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-18 21:06:56 -0400

    localstorage state

--- src/store.js
+++ src/store.js
@@ -4,12 +4,22 @@ import imagesReducer from "./reducers/images";
 import modalsReducer from "./reducers/modals";
 import thunk from "redux-thunk";
 
+import {loadState, saveState} from "./localStorage"; 
+
 const reducer = combineReducers({
 	streamReducer,
 	imagesReducer,
 	modalsReducer
 });
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const persistedState = loadState();
+const store = createStore(reducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(() => {
+	const state = store.getState();
+	saveState({
+		streamReducer: state.streamReducer
+	});
+});
 
 export default store;
